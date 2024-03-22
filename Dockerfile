b1533733ab9e3d57a6efa68875be3b7e0136f222
Ivan <imarcbenki@gmailcom>
2024-03-23 01:02:24 +0300

    Fix docker-compose

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /usr/src/personalised_nudges
 COPY . .
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
-CMD ["alembic", "upgrade", "head"]
+CMD ["sh", "-c", "alembic upgrade head"]
 #CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ from fastapi import FastAPI
 from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
 from src.operations.router import router as router_operation
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(
     title="Trading App"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:9000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 app.include_router(
