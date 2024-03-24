e478d219676eda3d862555e9249b356c5908e4e5
Ivan <ivan@MacBook-Air-Ivan.local>
2024-03-24 14:57:55 +0300

    vano upd

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -6,7 +6,7 @@ from src.auth.manager import get_user_manager
 from src.auth.models import User
 from src.config import SECRET_AUTH
 
-cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=900000, cookie_httponly=False,
+cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=900000, cookie_httponly=True,
                                    cookie_secure=False)
 
 
